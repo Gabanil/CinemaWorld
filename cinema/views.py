42133ebdc597c44c1a034698ec0dc396b644af18
andriimilevskyi <andrey.mil204@gmail.com>
2023-05-03 10:48:14 +0200

    Movie card. +index view

--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from .models import Movie
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
+class MovieAPIView(APIView):
+    def index(request):
+        movies = Movie.objects.all()
+        return render(request, "index.html", {"movies": movies})
+
+ #    шаблон вьюшки
+ # @swagger_auto_schema(
+    #     operation_summary="Get a dish by slug",
+    #     operation_description="<b>Retrieve a single dish by its unique slug.</b>",
+    #     responses={200: Dish_Serializer()},
+    #     tags=["Pages"])
+    # def get(self, request, slug):
+    #     try:
+    #         movies = Movie.objects.all()
+    #         # serializer = Dish_Serializer(dish, context={'request': request})
+    #         print(movies)
+    #         return Response(movies)
+    #     except Movie.DoesNotExist:
+    #         return Response({'error': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
