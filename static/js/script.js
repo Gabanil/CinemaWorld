c2dd49744654d4052fbad27aae529a57a7f7afc8
Maxym <ralphgucci3@gmail.com>
2023-05-07 16:46:37 +0300

    Adding sliders

---
+++ static/js/script.js
@@ -0,0 +1,41 @@
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+  
+    nxtBtn[i].addEventListener('click', () => {
+      const scrollAmount = containerWidth;
+      const scrollDuration = 500; // Задайте тривалість прокруту (у мілісекундах)
+  
+      smoothScroll(item, scrollAmount, scrollDuration);
+    });
+  
+    preBtn[i].addEventListener('click', () => {
+      const scrollAmount = -containerWidth;
+      const scrollDuration = 500; // Задайте тривалість прокруту (у мілісекундах)
+  
+      smoothScroll(item, scrollAmount, scrollDuration);
+    });
+  });
+  
+  function smoothScroll(element, scrollAmount, duration) {
+    const start = element.scrollLeft;
+    const startTime = performance.now();
+  
+    function step(timestamp) {
+      const currentTime = timestamp - startTime;
+      const scrollProgress = Math.min(currentTime / duration, 1);
+      const scrollPosition = start + scrollAmount * scrollProgress;
+  
+      element.scrollLeft = scrollPosition;
+  
+      if (currentTime < duration) {
+        window.requestAnimationFrame(step);
+      }
+    }
+  
+    window.requestAnimationFrame(step);
+}
